e06c3f40ef442af4155c5378b7dd99f65324292a
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-07-12 07:44:51 +0300

    manipulates elements in the DOM

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+//Remove element
 document.querySelector('main#main').remove();
+//Create new element
 const newHeader = document.createElement('h1');
+//Set id and innerHTML
 newHeader.id = 'victory';
-newHeader.innerHTML = 'YOUR-NAME is the champion';
+newHeader.innerHTML = 'Amwenya is the champion';
+//Append to body
 document.body.appendChild(newHeader);
